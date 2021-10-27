67d9ed9257a2320ced15d0760810a206bb9834a5
Antonella Rodríguez <82007150+anrrid@users.noreply.github.com>
2021-10-27 10:36:29 -0300

    Added Jquery in index

---
+++ myarchive.js
@@ -0,0 +1,15 @@
+$("#print").prepend(`<div><h1>Tu monedero virtual en un solo paso</h1>
+                  <p>Es muy sencillo comencémos</p>`);
+
+
+$('#btn1').click(function () {
+    window.location = 'operations.html';
+});
+
+
+$("button").css("background-color", "#4C8FFB");
+$("button").css("color", "#FFFFFF");
+$("button").css("border", "#3079ED solid");
+$("button").css("border-radius", "3px");
+$("button").css("font-size", "15px");
+$("button").css("box-shadow", "inset 0 1px 0 #80B0FB");
