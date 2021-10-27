fb28f11da220d735ea6dae2235e64598e748470f
Antonella Rodríguez <82007150+anrrid@users.noreply.github.com>
2021-10-26 21:34:32 -0300

    Revert "Merge branch 'main' of https://github.com/anrrid/cajero-simulador"
    This reverts commit f43196b4779071788a3a16e7df3bfa60eb323d5f, reversing
    changes made to a379c121212e78719785bb09a16ea0966e7c6a86.

--- app.js
+++ app.js
@@ -116,7 +116,7 @@ function saveOperation(operation) {
 const showOperation = () => {
 
     if (localStorage.getItem("transactionsList") == null) {
-        console.log("No hay operaciones registrado");
+        console.log("No hay residentes registrado");
     } else {
         const data = JSON.parse(localStorage.getItem("transactionsList"))
         console.log(data);
